4984dbe89d7993c8a671eadc6ada761b98967ef7
MARCELO RODRIGUES <marcelorodriguesweb@gmail.com>
2023-02-14 17:00:13 -0300

    Responsive

--- src/Components/Header/styles.tsx
+++ src/Components/Header/styles.tsx
@@ -11,8 +11,21 @@ export const HeaderContainer = styled.header`
     border: 2px solid ${(props) => props.theme.white};
     border-radius: 20px;
     margin: 0;
-
   }
+  @media screen and (max-width: 450px) {
+  .Header {
+    width: 50vw;
+    height: 80px;
+    border-radius: 10px;
+  }
+}
+  @media screen and (min-width: 450px) and (max-width: 700px) {
+  .Header {
+    width: 50vw;
+    height: 100px;
+    border-radius: 15px;
+   }
+}
 
   .ScoreDiv {
     margin: 0;
@@ -24,24 +37,56 @@ export const HeaderContainer = styled.header`
     height: 115px;
     background: ${(props) => props.theme.white};
     border-radius: 10px;
-
-    @media only screen and (max-width: 500px) {
-      width: 80px;
-      height: 57.5px;
-    }
   }
+  @media screen and (max-width: 450px) {
+  .ScoreDiv {
+    width: 80px;
+    height: 60px;
+  }
+}
+  @media screen and (min-width: 450px) and (max-width: 700px) {
+  .ScoreDiv {
+    width: 100px;
+    height: 80px;
+  }
+}
+
   .imgLogo {
     margin-left: 20px;
   }
+  @media screen and (max-width: 450px) {
+  .imgLogo img {
+    width: 60px;
+    height: auto;
+  }
+}
+  @media screen and (min-width: 450px) and (max-width: 700px) {
+    .imgLogo img {
+      width: 80px;
+      height: auto;
+    }
+  }
+
+
   .PointerScore {
     font-size: 3.5rem;
     font-weight: 700;
     color: ${(props) => props.theme["gray-500"]};
+  }
+  @media (max-width: 700px) {
+    .PointerScore {
+      font-size: 1.75rem;
+    }
   }
   .NameScore {
     font-size: 1rem;
     font-weight: 600;
     letter-spacing: 0.245rem;
     color: ${(props) => props.theme.score};
+  }
+  @media (max-width: 700px) {
+    .NameScore {
+      font-size: 0.75rem;
+    }
   }
 `
--- src/Home/styles.tsx
+++ src/Home/styles.tsx
@@ -11,6 +11,7 @@ export const Homecontainer = styled.main`
     justify-content: center;
     margin-top: 2rem;
   }
+
   .MainDiv {
     margin-top: 5rem;
   }
@@ -26,6 +27,11 @@ export const Homecontainer = styled.main`
     margin-top: -5rem;
     gap: 1rem;
   }
+  @media (max-width: 700px) {
+    .ButtonDiv {
+      margin-top: 0;
+    }
+  }
   .ButtonDiv button {
     width: 130px;
     height: 130px;
@@ -33,22 +39,69 @@ export const Homecontainer = styled.main`
     background: ${(props) => props.theme.white};
     cursor: pointer;
   }
+  @media screen and (min-width: 400px) and (max-width: 700px) {
+    .ButtonDiv button {
+      width: 100px;
+      height: 100px;
+    }
+  }
+  @media screen and (max-width: 400px) {
+    .ButtonDiv button {
+      width: 80px;
+      height: 80px;
+    }
+  }
+
   .TopDiv {
     margin-bottom: -5rem ;
+  }
+  @media screen and (min-width: 400px) and (max-width: 700px) {
+    .TopDiv {
+      margin-bottom: -3.5rem;
+    }
+  }
+  @media screen and (max-width: 400px) {
+    .TopDiv {
+      margin-bottom: -1rem;
+    }
   }
   .MiddleDiv {
     margin-bottom: -1rem;
     gap: 10rem;
   }
+  @media screen and (min-width: 400px) and (max-width: 700px) {
+    .MiddleDiv {
+      margin-bottom: 1rem;
+    }
+  }
+  @media screen and (max-width: 400px) {
+    .MiddleDiv {
+      margin-bottom: 2rem;
+      gap: 12rem;
+    }
+  }
   .BottomDiv {
     gap: 2.5rem;
     margin-bottom: -1.5rem;
+  }
+  @media screen and (min-width: 400px) and (max-width: 700px) {
+    .BottomDiv {
+      margin-bottom: 2rem;
+    }
+  }
+  @media screen and (max-width: 400px) {
+    .BottomDiv {
+      gap: 4.5rem;
+      margin-bottom: 2rem;
+    }
   }
   .ButtonScissors {
     border: solid 1rem ${(props) => props.theme["scissors-color"]};
     box-shadow: inset 0px 6px 3px ${(props) => props.theme["gray-shadow-400"]},
          0px 6px 3px 1px ${(props) => props.theme["gray-shadow-700"]};
   }
+
+
   .ButtonSpock {
     border: solid 1rem ${(props) => props.theme["spock-color"]};
     box-shadow: inset 0px 6px 3px ${(props) => props.theme["gray-shadow-400"]},
@@ -69,6 +122,62 @@ export const Homecontainer = styled.main`
     box-shadow: inset 0px 6px 3px ${(props) => props.theme["gray-shadow-400"]},
          0px 6px 3px 1px ${(props) => props.theme["gray-shadow-700"]};
   }
+
+  @media screen and (min-width: 400px) and (max-width: 700px) {
+    .ButtonScissors {
+      border: solid 0.75rem ${(props) => props.theme["scissors-color"]};
+    }
+    .ButtonLizard {
+      border: solid 0.75rem ${(props) => props.theme["lizard-color"]};
+    }
+    .ButtonSpock {
+      border: solid 0.75rem ${(props) => props.theme["spock-color"]};
+    }
+    .ButtonPaper {
+      border: solid 0.75rem ${(props) => props.theme["paper-color"]};
+    }
+    .ButtonRock {
+      border: solid 0.75rem ${(props) => props.theme["rock-color"]};
+    }
+  }
+
+  @media screen and (max-width: 400px) {
+    .ButtonScissors {
+      border: solid 0.5rem ${(props) => props.theme["scissors-color"]};
+    }
+    .ButtonScissors img {
+      width: 50%;
+    }
+
+    .ButtonSpock {
+      border: solid 0.5rem ${(props) => props.theme["spock-color"]};
+    }
+    .ButtonSpock img {
+      width: 50%;
+    }
+
+    .ButtonPaper {
+      border: solid 0.5rem ${(props) => props.theme["paper-color"]};
+    }
+    .ButtonPaper img {
+      width: 50%;
+    }
+
+    .ButtonRock {
+      border: solid 0.5rem ${(props) => props.theme["rock-color"]};
+    }
+    .ButtonRock img {
+      width: 50%;
+    }
+
+    .ButtonLizard {
+      border: solid 0.5rem ${(props) => props.theme["lizard-color"]};
+    }
+    .ButtonLizard img {
+      width: 50%;
+    }
+  }
+
   .RulesDiv {
     display: flex;
     width: 100vw;
@@ -83,8 +192,14 @@ export const Homecontainer = styled.main`
     color: ${(props) => props.theme.white};
     letter-spacing: 2px;
     cursor: pointer;
-
   }
+  @media (max-width: 700px) {
+    .RulesDiv button {
+      width: 80px;
+      height: 35px;
+      font-weight: 400;
 
+    }
+  }
 
 `
